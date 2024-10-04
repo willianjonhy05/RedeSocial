9f2b7d9e78ddf8ed3b7655e868fc33784f4d6795
willianjohny05 <willianjohny05@gmail.com>
2024-10-04 20:03:44 -0300

    Inclui a FlatList

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import AsyncStorage from '@react-native-async-storage/async-storage'; // Não se esqueça de limpar o storage no logout
+import { FlatList } from 'react-native-gesture-handler';
 
 export default function HomeScreen({ navigation, setIsLoggedIn }) {
     const [saldo, setSaldo] = useState(0.25);
+    const [movimentacoes, setMovimentacoes] = useState([]);
+    const [novaMovimentacao, setNovaMovimentacao] = useState('');
+
+    // Carregar movimentações do AsyncStorage quando a tela é montada
+    useEffect(() => {
+        const carregarMovimentacoes = async () => {
+            const salvarMovimentacoes = await AsyncStorage.getItem('MovimentacoesFinanceiras');
+
+            if (salvarMovimentacoes) {
+                setMovimentacoes(JSON.parse(salvarMovimentacoes));
+            }
+        };
+        carregarMovimentacoes();
+    }, []);
+
+    const AddMovimentacoes = async () => {
+        if (novaMovimentacao.trim() === '') return; // Prevenir entradas vazias
+        const atualizarMovimentacoes = [...movimentacoes, novaMovimentacao];
+        setMovimentacoes(atualizarMovimentacoes);
+        setNovaMovimentacao('');
+        await AsyncStorage.setItem('MovimentacoesFinanceiras', JSON.stringify(atualizarMovimentacoes));
+    };
 
     const handleLogout = async () => {
         await AsyncStorage.removeItem('loggedIn'); // Limpa o status de login no AsyncStorage
@@ -18,12 +41,12 @@ export default function HomeScreen({ navigation, setIsLoggedIn }) {
             <Text style={[styles.saldo, { color: saldo > 0 ? 'green' : 'red' }]}>
                 R$ {saldo.toFixed(2)}
             </Text>
-            <Text style={styles.label}>Receitas Totais: R$ { }</Text>
-            <Text style={styles.label}>Despesas Totais: R$ { }</Text>
+            <Text style={styles.label}>Receitas Totais: R$ {}</Text>
+            <Text style={styles.label}>Despesas Totais: R$ {}</Text>
             <StatusBar style="auto" />
 
             <View style={styles.containerButton}>
-                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Receitas')}>
+                <TouchableOpacity style={styles.button} onPress={AddMovimentacoes}>
                     <Text style={styles.textButton}>Nova Receita</Text>
                 </TouchableOpacity>
                 <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Despesas')}>
@@ -33,6 +56,23 @@ export default function HomeScreen({ navigation, setIsLoggedIn }) {
                     <Text style={styles.textButton}>Logout</Text>
                 </TouchableOpacity>
             </View>
+
+            <View style={styles.ContainerList}>
+                <TextInput
+                    style={styles.Input}
+                    placeholder='Nova Entrada'
+                    value={novaMovimentacao}
+                    onChangeText={setNovaMovimentacao}
+                />
+            </View>
+
+
+            <FlatList
+                data={movimentacoes}
+                renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+                keyExtractor={(item, index) => index.toString()}
+            />
+
         </View>
     );
 }
@@ -62,7 +102,7 @@ const styles = StyleSheet.create({
     containerButton: {
         display: 'flex',
         flexDirection: 'row',
-        gap: 10
+        gap: 10,
     },
     button: {
         width: '32%',
@@ -76,5 +116,16 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontWeight: 'bold',
         fontSize: 15,
+    },
+    ContainerList: {
+        justifyContent: 'center',
+        marginTop: 20,
+    },
+    Input: {
+        borderWidth: 1,
+        borderColor: '#ccc',
+        padding: 10,
+        marginBottom: 10,
+        borderRadius: 5,
     },
 });
